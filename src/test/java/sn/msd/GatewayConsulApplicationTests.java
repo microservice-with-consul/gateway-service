ce63ba1b05844be941387e8cef2d8b2f3ffea72e
Mamadou seydou DIALLO <diallodiery301gmail.com>
2022-11-27 22:44:16 +0000

    boostrap gateway with consul

---
+++ src/test/java/sn/msd/GatewayConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.msd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
